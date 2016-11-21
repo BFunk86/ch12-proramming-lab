b188e262d2291f5b7f55928045529f69e5db993a
Brandon Young <fultoncountycommercialcleaning@gmail.com>
2016-11-21 01:34:11 -0500

    Added project description to Driver class

--- src/DriverYoung.java
+++ src/DriverYoung.java
@@ -3,9 +3,10 @@
  * Data Structures
  *
  * Project Description
-
-    Implement an unsorted array-based dictionary (table) based on the given DictionaryInterface. Allow the array to expand as necessary during execution. Write a test program to test the unsorted array-base table class you wrote.
-
+ *
+ * Implement an unsorted array-based dictionary (table) based on the given DictionaryInterface. Allow the array to
+ * expand as necessary during execution. Write a test program to test the unsorted array-base table class you wrote.
+ *
  * @author Brandon Young
  */
 // File Name: DriverYoung.java
